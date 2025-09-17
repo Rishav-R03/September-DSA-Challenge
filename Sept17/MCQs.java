1723fd4b8980d420bab5712d7753fe2d21a1ab2d
Rishav-R03 <rishav042023@gmail.com>
2025-09-17 16:06:34 +0530

    mcqs

---
+++ Sept17/MCQs.java
@@ -0,0 +1,10 @@
+
+public class MCQs {
+    public static void main(String[] args) {
+        byte x = 64;
+        int result1 = x >> 1;
+        int result2 = x << 1;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
